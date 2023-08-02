e0ee5084a5525f575186248563752451e54a04e8
JackOne850 <118886732+JackOne850@users.noreply.github.com>
2023-08-02 21:29:56 +0200

    secondo commit di prova
    seconda descrizione di prova

--- src/main/java/com/jackone/mycoolapp/MycoolappApplication.java
+++ src/main/java/com/jackone/mycoolapp/MycoolappApplication.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class MycoolappApplication {
 //commento di prova
+	//secondo commento di prova
 	public static void main(String[] args) {
 		SpringApplication.run(MycoolappApplication.class, args);
 	}
